7e7bf329fa732159090b5ecb4ad5a8e729a9ea41
Benjamin <benjaminbout@hotmail.fr>
2017-01-10 16:06:44 +0100

    Remplissage des listes d'examens

--- app/src/main/java/techmed/guide/data/Generator.java
+++ app/src/main/java/techmed/guide/data/Generator.java
@@ -55,10 +55,10 @@ public class Generator {
                 "Dans le bilan des cardiopathies ischémiques, le scanner cardiaque est, actuellement, la technique non invasive, la plus performante pour l’exploration des artères coronaires. Par son acquisition volumique, le scanner s’affirme comme le moyen d’explorer en ambulatoire de façon reproductible non seulement les artères coronaires mais aussi toutes les structures cardiaques. Le scanner cardiaque est entrain de devenir un examen de routine qui trouve progressivement sa place parmi les différentes explorations cardiaques.   \n" +
                 "\n"));
 
-        items.add(new Examen("Echographie-Doppler des artères", "Visualiser les tissus et la cinétique", "L'écho-Doppler artériel permet de visualiser les artères et les flux sanguins qui les parcourent, en temps réel.C'est l'examen complémentaire de référence pratiqué dans le bilan des artérites des membres inférieurs et dans leur suivi.\n" +
+        items.add(new Examen("Echographie-Doppler des artères", R.drawable.echo_dopler_artères, "Visualiser les tissus et la cinétique", "L'écho-Doppler artériel permet de visualiser les artères et les flux sanguins qui les parcourent, en temps réel.C'est l'examen complémentaire de référence pratiqué dans le bilan des artérites des membres inférieurs et dans leur suivi.\n" +
                 "L'échographoe-Doppler se pratique sur les principaux troncs artériels qui peuvent être atteint par la maladie athéromateuse, à savoir les carotides, les artères des membres inférieurs et l'aorte. La personne est en position allongée et chaque artère est explorée séparément avec la sonde d'échographie."));
 
-        items.add(new Examen("Echographie transoesophagienne", "Visualiser parfaitement l’ensemble des tissus qui constituent le cœur", "Cet examen spécialisé, explore le cœur,\n" +
+        items.add(new Examen("Echographie transoesophagienne", R.drawable.echo_transoeuso, "Visualiser parfaitement l’ensemble des tissus qui constituent le cœur", "Cet examen spécialisé, explore le cœur,\n" +
                 "les valves et les cavités cardiaques, selon les\n" +
                 "pour visualiser votre œsophage ou votre estomac).\n" +
                 "Il s’agit en fait d’une voie privilégiée pour obtenir des images du cœur puisque l’œsophage\n" +
@@ -66,11 +66,11 @@ public class Generator {
                 "ce qui n’est pas toujours le cas en échographie trans-thoracique.\n" +
                 "L’échographie trans-œsophagienne permet l’étude du cœur et de l’aorte, et surtout des petites structures cardiaques qui sont parfois inaccessibles non seulement à l’échographie trans-thoracique mais aussi à d’autres méthodes d’imagerie radiologique (scanner, par exemple). Les renseignements obtenus vont permettre de confirmer ou d’éliminer un diagnostic qui était suspecté (infection de valve, caillot, problème à l’aorte...), d’évaluer la sévérité d’une atteinte des valves (valve « d’origine » ou prothèse), de guider certaines décisions d’intervention."));
 
-        items.add(new Examen("Echographie-Doppler des veines", "Analyser la structure des veines et le flux sanguin", "L’écho-Doppler est l’examen de base permettant l’exploration du réseau veineux. Il est indispensable avant tout traitement de varices, mais il est aussi parfois pratiqué en urgence en cas de suspicion de thrombose veineuse ou « phlébite » (obstruction de veines par un caillot).\n" +
+        items.add(new Examen("Echographie-Doppler des veines", R.drawable.echo_doppler_veines, "Analyser la structure des veines et le flux sanguin", "L’écho-Doppler est l’examen de base permettant l’exploration du réseau veineux. Il est indispensable avant tout traitement de varices, mais il est aussi parfois pratiqué en urgence en cas de suspicion de thrombose veineuse ou « phlébite » (obstruction de veines par un caillot).\n" +
                 "L'écho-Doppler veineux permet en temps réel de visualiser les veines ainsi que les flux sanguins qui les parcourent. Il se pratique essentiellement aux membres inférieurs, sur les trajets veineux des mollets, des cuisses et de l’aine, en remontant parfois même jusqu’à l’abdomen (pour explorer les veines iliaques et la veine cave inférieure).\n" +
                 "Plus rarement, ce sont les veines du membre supérieur qui sont explorées, par exemple en cas de suspicion de thrombose veineuse du bras."));
 
-        items.add(new Examen("Scintigraphie cardiaque", "Evaluer la fonction du muscle cardiaque, donne des renseignements sur ses capacités de contraction", "Le but de cet examen est d’évaluer la vascularisation régionale du muscle cardiaque à l’effort et le plus souvent au repos. \n" +
+        items.add(new Examen("Scintigraphie cardiaque", R.drawable.scintigraphie, "Evaluer la fonction du muscle cardiaque, donne des renseignements sur ses capacités de contraction", "Le but de cet examen est d’évaluer la vascularisation régionale du muscle cardiaque à l’effort et le plus souvent au repos. \n" +
                 "Cet examen permet de déterminer une lésion du muscle cardiaque (infarctus du myocarde) ou un flux sanguin insuffisant au cours de l’effort (ischémie myocardique). \n" +
                 "Ce test est prescrit par votre cardiologue, compte tenu de vos antécédents, de vos facteurs de risque vasculaire ou de votre symptomatologie."));
 
